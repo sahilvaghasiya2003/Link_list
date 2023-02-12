c77ef554260564557a705a8cabf6e99fe22a8e92
sahilvaghasiya2003 <121418927+sahilvaghasiya2003@users.noreply.github.com>
2023-02-12 09:30:50 +0530

    Add files via upload

---
+++ 1_insert_in_between_linklist.c
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void print(struct Node *ptr)
+{
+    while (ptr != NULL)
+    {
+        printf("Element: %d\n", ptr->data);
+        ptr = ptr->next;
+    }
+}
+
+// struct Node *add_first(struct Node *head, int data)
+// {
+//     struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+//     ptr->data = data;
+//     ptr->next = head;
+//     head = ptr;
+// }
+
+struct Node *add_between(struct Node *head, int data, int index)
+{
+    struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *p = head;
+    int i = 0;
+
+    while (i != index - 1)
+    {
+        p = p->next;
+        i++;
+    }
+    ptr->data = data;
+    ptr->next = p->next;
+    p->next = ptr;
+    return head;
+}
+
+int main()
+{
+    struct Node *head;
+    struct Node *second;
+    struct Node *third;
+    struct Node *fourth;
+
+    head = (struct Node *)malloc(sizeof(struct Node));
+    second = (struct Node *)malloc(sizeof(struct Node));
+    third = (struct Node *)malloc(sizeof(struct Node));
+    fourth = (struct Node *)malloc(sizeof(struct Node));
+
+    head->data = 10;
+    head->next = second;
+
+    second->data = 20;
+    second->next = third;
+
+    third->data = 30;
+    third->next = fourth;
+
+    fourth->data = 40;
+    fourth->next = NULL;
+
+    // head = add_first(head, 50);
+    head = add_between(head, 50, 3);
+    head = add_between(head, 100, 2);
+    print(head);
+
+    return 0;
+}
